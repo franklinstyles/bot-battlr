9524deb4c80a1de0d28069463ee2ce3284794fdc
franklin ndegwa <franklin.ndegwa@student.moringaschool.com>
2024-08-11 22:28:00 +0300

    changes to db.json and added some components

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from '../App.jsx'
-import './index.css'
+import React from "react";
+import BotsPage from "./BotsPage";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+function App() {
+  return (
+    <div className="App">
+      <BotsPage />
+    </div>
+  );
+}
+
+export default App;
